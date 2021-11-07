7dd4382e6507074e0a6b9c07161830f12e4a9baa
Noah Jones <noahtjones@gmail.com>
2021-11-07 13:26:00 -0500

    created electron app

---
+++ adao-vote/main.js
@@ -0,0 +1,55 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+let addKey;
+
+// listen for app to be ready
+
+app.on('ready', function(){
+    //create window
+    mainWindow = new BrowserWindow({});
+    // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    // build menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle stuff
+function createAddKey(){
+    
+}
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label: 'Add Secret Key',
+                click(){
+                    
+                }
+            },
+            {
+                label: 'Clear Keys'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
